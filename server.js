12e2dc16b5b07093ed7e705e821a34b76f0e400b
richard-dinh <richarddinh@gmail.com>
2020-03-24 14:55:57 -0700

    set up server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require('express')
+const {join} = require('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public', 'build')))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.listen(process.env.PORT || 3001)
