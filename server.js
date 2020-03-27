412e2ce9a6a9d3b7aca28db6b167874c7f6b8808
richard-dinh <richarddinh@gmail.com>
2020-03-27 09:58:36 -0700

    readded static.json

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use(require('./routes'))
-app.get('/*', (requestt, response) => {
+app.get('/*', (request, response) => {
   response.sendFile(join(__dirname, 'client', 'build', 'index.html'))
 })
 
