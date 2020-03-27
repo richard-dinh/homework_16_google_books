17a797cafd00ec7a985c6938a1588e1c84b2e766
richard-dinh <richarddinh@gmail.com>
2020-03-27 09:42:10 -0700

    fixed server file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {join} = require('path')
 
 const app = express()
 
-app.use(express.static(join(__dirname, 'public', 'build')))
+app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
